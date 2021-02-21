15b01170af95158c1244c6a3d98cfebd95a5daf8
oday abu saleh <adlioday@gmail.com>
2021-02-21 22:51:39 +0000

    fix cloudianry

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,7 +1,8 @@
 import flatpickr from "flatpickr";
 
 const initFlatpickr = () => {
-  flatpickr(".datepicker", {});
+  // flatpickr(".datepicker", {});
+  flatpickr(".datetimepicker", {});
 }
 
 export { initFlatpickr };
